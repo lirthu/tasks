97d41625a66e478392b33db2213231b4cf99dedb
lirthu <lirthu777@gmail.com>
2026-09-18 15:55:12 +0300

    adding function + checking

--- weather.py
+++ weather.py
@@ -1,4 +1,8 @@
 import requests
 
-response = requests.get(f'https://wttr.in/Москва?M?T?&lang=ru')
-print(response.text)
+def get_weather():
+    response = requests.get(f'https://wttr.in/Москва?M?T?&lang=ru')
+    print(response.text)
+
+if __name__ == '__main__':
+    get_weather()
