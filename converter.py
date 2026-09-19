b29596b7ff391be9d260f85498c0a4468a71c9f1
lirthu <lirthu777@gmail.com>
2026-09-19 23:22:52 +0300

    naming + getting data function

--- converter.py
+++ converter.py
@@ -1,17 +1,23 @@
 import argparse
 import requests
 
-response = requests.get('https://www.cbr-xml-daily.ru/daily_json.js')
-response_json = response.json()
+def get_json_data(link):
+    response = requests.get(link)
+    data_json = response.json()
+    return data_json
 
-def convert(amount, in_valute):
-    value_in_valute = response_json['Valute'][in_valute]['Value']
-    total = amount * value_in_valute
+def converter_valute(amount, valute_name, json_data):
+    get_value = json_data['Valute'][valute_name]['Value']
+    total = amount * get_value
     return total
 
 if __name__ == '__main__':
+
+    get_link = 'https://www.cbr-xml-daily.ru/daily_json.js'
+
     parser = argparse.ArgumentParser(description='Valute to RUB')
     parser.add_argument('amount', type=int, help='amount of valute to convert')
     parser.add_argument('valute', type=str, help='type of valute to convert into rubles')
     args = parser.parse_args()
-    print(convert(args.amount, args.valute))
+
+    print(converter_valute(args.amount, args.valute, get_json_data(get_link)))
